eef0a053284459bd3f058cf2750f0c577d416b47
Christopher Goltz <christopher.goltz@outlook.de>
2021-05-28 12:31:59 +0200

    initial commit

---
+++ Imago/Imago/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Imago.Services;
+using Imago.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imago
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Imago/Imago/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Imago.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Imago.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
