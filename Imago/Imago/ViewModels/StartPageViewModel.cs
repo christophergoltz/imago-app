d75d6fbc1bb28a996a4a5b96ab8fd8f0e0bcb07b
Christopher Goltz <christopher.goltz@outlook.de>
2021-06-10 16:11:47 +0200

    fixed binding issues in console

--- Imago/Imago/ViewModels/StartPageViewModel.cs
+++ Imago/Imago/ViewModels/StartPageViewModel.cs
@@ -68,7 +68,6 @@ namespace Imago.ViewModels
                     {
                         tableInfoModel.State = TableInfoState.Error;
                         Debug.WriteLine(e);
-                //        throw;
                     }
 
                 }
