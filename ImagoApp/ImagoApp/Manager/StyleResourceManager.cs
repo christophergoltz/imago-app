634338ec968f3d9eb35cc8b0ad97b777e42db266
Christopher Goltz <christopher.goltz@outlook.de>
2021-07-23 10:42:07 +0200

    added styles

---
+++ ImagoApp/ImagoApp/Manager/StyleResourceManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImagoApp.Manager
+{
+    public static class StyleResourceManager
+    {
+        public static T TryGetValue<T>(string key)
+        {
+            Xamarin.Forms.Application.Current.Resources.TryGetValue(key, out var value);
+            if (value != null)
+                return (T) value;
+            
+            return default(T);
+        }
+
+        public static void SetValue(string key, object value)
+        {
+            //check main resources
+            if (Xamarin.Forms.Application.Current.Resources.ContainsKey(key))
+            {
+                Xamarin.Forms.Application.Current.Resources[key] = value;
+                return;
+            }
+
+            foreach (var dictionary in Xamarin.Forms.Application.Current.Resources.MergedDictionaries)
+            {
+                foreach (var VARIABLE in dictionary)
+                {
+                    
+                }
+
+                if (dictionary.ContainsKey(key))
+                {
+                    dictionary[key] = value;
+                    return;
+                }
+            }
+        }
+    }
+}
