3df54917c8dde4de6ad88a19070370042eacc065
Christopher Goltz <christopher.goltz@outlook.de>
2021-07-24 12:41:26 +0200

    added basic exceptionhandling for maximize app on start

--- ImagoApp/ImagoApp.UWP/MainPage.xaml.cs
+++ ImagoApp/ImagoApp.UWP/MainPage.xaml.cs
@@ -18,27 +18,41 @@ namespace ImagoApp.UWP
 
         private static void MaximizeWindowOnLoad()
         {
-            // Get how big the window can be in epx.
-            var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            try
+            {
+                // Get how big the window can be in epx.
+                var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
 
-            ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+                ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
+                ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            }
+            catch (Exception)
+            {
+                //ignored
+            }
         }
 
         private static void MaximizeWindowOnLoad2()
         {
-            var view = DisplayInformation.GetForCurrentView();
+            try
+            {
+                var view = DisplayInformation.GetForCurrentView();
 
-            // Get the screen resolution (APIs available from 14393 onward).
-            var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
+                // Get the screen resolution (APIs available from 14393 onward).
+                var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
 
-            // Calculate the screen size in effective pixels. 
-            // Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
-            var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
-            var bounds = new Size(resolution.Width / scale, resolution.Height / scale);
+                // Calculate the screen size in effective pixels. 
+                // Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
+                var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
+                var bounds = new Size(resolution.Width / scale, resolution.Height / scale);
 
-            ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+                ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
+                ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            }
+            catch (Exception)
+            {
+                //ignored
+            }
         }
     }
 }
