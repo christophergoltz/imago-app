049fa22c6da6c704059c9328068f24ebc308bfa1
Christopher Goltz <christopher.goltz@outlook.de>
2021-07-30 10:19:35 +0200

    removed IncreaseConstants

--- ImagoApp.Application/Services/IncreaseCalculationService.cs
+++ ImagoApp.Application/Services/IncreaseCalculationService.cs
@@ -19,6 +19,15 @@ namespace ImagoApp.Application.Services
 
     public class IncreaseCalculationService : IIncreaseCalculationService
     {
+        private static readonly int[] ExperienceLookup = { 2, 3, 5, 8, 12, 17, 23, 30, 38, 47 };
+       
+        public enum IncreaseType
+        {
+            Attribute,
+            SkillGroup,
+            Skill
+        }
+        
         /// <summary>
         /// Recalculates the increase infos
         /// </summary>
@@ -33,7 +42,7 @@ namespace ImagoApp.Application.Services
             if (increasableBase is SkillModel skillModel)
                 totalExperience += skillModel.CreationExperience;
 
-            var info = IncreaseConstants.GetIncreaseInfo(IncreaseConstants.IncreaseType.Attribute, totalExperience);
+            var info = GetIncreaseInfo(IncreaseType.Attribute, totalExperience);
 
             var oldIncreaseValue = increasableBase.IncreaseValue;
             increasableBase.IncreaseValue = info.IncreaseLevel;
@@ -42,6 +51,75 @@ namespace ImagoApp.Application.Services
 
             return info.IncreaseLevel - oldIncreaseValue;
         }
+        
+        private static int GetExperienceRequiredForNextIncrease(IncreaseType increaseType, int increaseValue)
+        {
+            var index = 0;
 
+            switch (increaseType)
+            {
+                case IncreaseType.Attribute:
+                    {
+                        var indexPart = (double)increaseValue / 10;
+                        index = (int)Math.Floor(indexPart) - 2;
+                        break;
+                    }
+                case IncreaseType.SkillGroup:
+                    {
+                        var indexPart = (double)increaseValue / 5;
+                        index = (int)Math.Floor(indexPart) + 2;
+                        break;
+                    }
+                case IncreaseType.Skill:
+                    {
+                        var indexPart = (double)increaseValue / 15;
+                        index = (int)Math.Floor(indexPart);
+                        break;
+                    }
+            }
+
+            if (index < 0)
+                index = 0;
+
+            if (index > 9)
+                index = 9;
+
+            return ExperienceLookup[index];
+        }
+
+        public static int GetExperienceRequiredForLevel(IncreaseType increaseType, int increaseValue)
+        {
+            var currentIncrease = 0;
+            var requiredExperience = 0;
+            while (currentIncrease < increaseValue)
+            {
+                var costForNextIncrease = GetExperienceRequiredForNextIncrease(increaseType, currentIncrease);
+                requiredExperience += costForNextIncrease;
+                currentIncrease++;
+            }
+
+            return requiredExperience;
+        }
+
+        private static (int IncreaseLevel, int LeftoverExperience, int ExperienceForNextIncrease) GetIncreaseInfo(IncreaseType increaseType, int totalExperience)
+        {
+            var leftoverExperience = totalExperience;
+            var increaseValue = 0;
+            int costForNextIncrease;
+
+            while (true)
+            {
+                costForNextIncrease = GetExperienceRequiredForNextIncrease(increaseType, increaseValue);
+
+                if (leftoverExperience < costForNextIncrease)
+                    break; //cant increase
+
+                //increase
+                leftoverExperience -= costForNextIncrease;
+                increaseValue++;
+            }
+
+            return (increaseValue, leftoverExperience, costForNextIncrease);
+        }
     }
 }
