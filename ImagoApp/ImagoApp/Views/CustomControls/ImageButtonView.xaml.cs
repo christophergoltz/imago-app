353062a751280dfeaee042ce2f0d365977f51cb6
Christopher Goltz <christopher.goltz@outlook.de>
2021-11-18 16:36:24 +0100

    added ImageButtonView

---
+++ ImagoApp/ImagoApp/Views/CustomControls/ImageButtonView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImagoApp.Views.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageButtonView : ContentView
+    {
+        public ImageButtonView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text), // the name of the bindable property
+            typeof(string), // the bindable property type
+            typeof(ImageButtonView));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(
+            nameof(ImageSource), // the name of the bindable property
+            typeof(ImageSource), // the bindable property type
+            typeof(ImageButtonView));
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create(
+            nameof(Command), // the name of the bindable property
+            typeof(ICommand), // the bindable property type
+            typeof(ImageButtonView));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+    }
+}
