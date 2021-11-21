f88c38e9522d9db0a6b3e0efe914d30f422bda92
Christopher Goltz <christopher.goltz@outlook.de>
2021-11-21 11:09:36 +0100

    added test fonts

--- ImagoApp/ImagoApp/AssemblyInfo.cs
+++ ImagoApp/ImagoApp/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("hvd_edding.otf", Alias = "B")]
+[assembly: ExportFont("Kalam-Regular.ttf", Alias = "C")]
+[assembly: ExportFont("palitoon.otf", Alias = "D")]
