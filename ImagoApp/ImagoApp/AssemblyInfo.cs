ec2dcf544060603fa95a3a145ef25349672ce520
Christopher Goltz <christopher.goltz@outlook.de>
2021-11-21 12:55:30 +0100

    fixed font size

--- ImagoApp/ImagoApp/AssemblyInfo.cs
+++ ImagoApp/ImagoApp/AssemblyInfo.cs
@@ -2,6 +2,4 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("hvd_edding.otf", Alias = "B")]
-[assembly: ExportFont("Kalam-Regular.ttf", Alias = "C")]
-[assembly: ExportFont("palitoon.otf", Alias = "D")]
+[assembly: ExportFont("palitoon.otf", Alias = "Palitoon")]
